9a166217c4f6463239f8595eeff1e997e8feb6f3
СЕГА <skuhronak@gmail.com>
2025-10-27 23:05:08 +0300

    Task0

---
+++ Tyuiu.KuharonakSV.Sprint1.Task0.V9.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.KuharonakSV.Sprint1.Task0.V9.Lib;
+namespace Tyuiu.KuharonakSV.Sprint1.Task0.V9.Test
+
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(3, res);
+
+
+
+
+
+        }
+    }
+}
---
+++ Tyuiu.KuharonakSV.Sprint1.Task0.V9/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.KuharonakSV.Sprint1.Task0.V9.Lib;
+
+internal class Program
+
+
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #0 | Выполнил: Кухаронак С. В. | СМАРТб-25-1 ";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Создания итогового решения по спринту                             *");
+        Console.WriteLine("* Задание #0                                                              *");
+        Console.WriteLine("* Вариант #9                                                              *");
+        Console.WriteLine("* Выполнил: Кухаронак Сергей Витальевич |СМАРТб-25-1                      *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая вычисляет выражение (9/3)*4/3/2+1           *");
+        Console.WriteLine("*  и печатает результат на экране                                         *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("(9/3)*4/3/2+1                                                             *");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(ds.Calculate());
+        Console.ReadLine();
+    }
+}
