1263d1a15ab7c29121bb76e35149299a851972b5
СЕГА <skuhronak@gmail.com>
2025-10-28 21:46:41 +0300

    Task3

---
+++ Tyuiu.KuharonakSV.Sprint1.Task3.V13.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.KuharonakSV.Sprint1.Task3.V13.Lib;
+namespace Tyuiu.KuharonakSV.Sprint1.Task3.V13.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+             DataService ds = new DataService();
+            double x = 453;
+            double a = (int)(x / 100);
+            double b = (int)((x % 100) / 10);
+            double c = (int)x % 10;
+            int wait = 60;
+            var res = ds.MultiplyOfDigits(x);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KuharonakSV.Sprint1.Task3.V13/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.KuharonakSV.Sprint1.Task3.V13.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #1 | Выполнил: Кухаронак С В | СМАРТБ-25-1;";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема:  Организация ввода/вывода в консольных приложениях                *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #13                                                             *");
+        Console.WriteLine("* Выполнил: Кухароеак Сергей Витальевич | СМАРТБ-25-1                     *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая позволяет найти произведение цифр,          *");
+        Console.WriteLine("* заданного трехзначного числа                                            *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        double x = 634;
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Исходное число--"+x);
+        Console.WriteLine("Произведение="+ds.MultiplyOfDigits(x));
+
+        Console.ReadLine();
+    }
+}
