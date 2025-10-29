9e646493f3ad3514e2acaede09da40ae10f929d6
СЕГА <skuhronak@gmail.com>
2025-10-29 13:26:44 +0300

    Task5

---
+++ Tyuiu.KuharonakSV.Sprint1.Task5.V4.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.KuharonakSV.Sprint1.Task5.V4.Lib;
+namespace Tyuiu.KuharonakSV.Sprint1.Task5.V4.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService(); 
+            int x = 13257;
+            
+            double res = ds.SecondsToHours(x);
+            int result = Convert.ToInt32(res);
+            int wait = 3;
+            
+            Assert.AreEqual(result, wait); 
+        }
+    }
+}
