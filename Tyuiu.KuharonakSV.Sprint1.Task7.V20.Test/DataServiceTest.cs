425f979c878c19be3b8ccbdb76943a08a00b9de5
СЕГА <skuhronak@gmail.com>
2025-10-30 21:37:27 +0300

    Task7

---
+++ Tyuiu.KuharonakSV.Sprint1.Task7.V20.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.KuharonakSV.Sprint1.Task7.V20.Lib;
+namespace Tyuiu.KuharonakSV.Sprint1.Task7.V20.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 2;
+            double z = 4.917;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(z, res);
+        }
+    }
+}
---
+++ Tyuiu.KuharonakSV.Sprint1.Task7.V20/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.KuharonakSV.Sprint1.Task7.V20.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #1 | Выполнил Кухаронак С.В. | СМАРТБ-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* спринт #1                                                               *");
+        Console.WriteLine("* Тема : Добавление к решению итоговых проектов по спринту                *");
+        Console.WriteLine("* Задание #7                                                              *");
+        Console.WriteLine("* Вариант #20                                                             *");
+        Console.WriteLine("* Выполнил: Кухаронак Сергей Витальевич | СМАРТБ-25-1                     *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая вычисляет математическое выражение          *");
+        Console.WriteLine("* по исходным значениям данных, вводимых пользователем.                   *");
+        Console.WriteLine("* Ответ округлите до 3 знаков после запятой.                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        double x = 0;
+        double y = 0;
+
+
+        Console.WriteLine("Введите значение x: ");
+        x = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите значение y: ");
+        x = Convert.ToDouble(Console.ReadLine());
+
+
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+
+        Console.WriteLine(ds.Calculate(x, y));
+
+        Console.ReadLine();
+    }
+}
